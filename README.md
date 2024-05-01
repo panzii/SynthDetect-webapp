e3280d7440a546afe2d7ef4c4bef0cde3a9eba14
Saurabh Pande <saurabhpande74@gmail.com>
2024-05-01 05:42:02 -0400

    website fix

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SynthDetect-webapp
 Website for ML project
+
+## Data we used consisted of more than 100k images so those are not included in the repo
--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+
 
 app = Flask(__name__)
 
 
-@app.route("/")
+
+
+
+@app.route("/", methods=['GET'])
 def home():
     return render_template('home.html')
 
-
-@app.route("/demo")
+@app.route("/demo", methods=['GET'])
 def demo():
     return render_template('demo.html')
 
-@app.route("/team")
-
+@app.route("/team", methods=['GET'])
 def team():
     return render_template('team.html')
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
